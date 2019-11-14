64bc36dfa62019205fbc1510d492959a52dac81e
Priyank <gohilpriyank9@gmail.com>
2019-11-14 07:45:55 +0530

    working with hot reload in development mode

---
+++ webpack.development.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist"),
+        publicPath: ""
+    },
+    mode: "development",
+    devServer: {
+        contentBase: path.resolve(__dirname, "./dist"),
+        index: 'index.html',
+        port: 9001
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                '**/*',
+                path.join(process.cwd(), 'build/**/*')
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            title: "Hello World",
+            template: "src/index.hbs",
+            description: 'Some Description'
+        })
+    ]
+}
+;
